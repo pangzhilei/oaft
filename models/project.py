4b5a8afdeb80e66e2513cbcc998dfd4c599a3abc
pangzhilei <pangzhilei@foresthunder.cn>
2017-12-29 19:41:30 +0800

    会传递值了

---
+++ models/project.py
@@ -0,0 +1,58 @@
+class Project:
+    def __init__(self,title,agency,boss,price,tax,bonus,costforcode,costforweb,costformanager,costforexpress,costforserver,costforcdn,costfromaintenance,costfor3rdparty,manager,flow,prototype,sow,sob,expressno,debugdate,deadline,contract,invoice,getmoney,close,info):
+        self.title=title
+        self.agency=agency
+        self.boss=boss
+        self.price=price
+        self.tax=tax
+        self.bonus=bonus
+        self.costforcode=costforcode
+        self.costforweb=costforweb
+        self.costformanager=costformanager
+        self.costforexpress=costforexpress
+        self.costforserver=costforserver
+        self.costforcdn=costforcode
+        self.costfromaintenance=costfromaintenance
+        self.costfor3rdparty=costfor3rdparty
+        self.manager=manager
+        self.flow=flow
+        self.prototype=prototype
+        self.sow=sow
+        self.sob=sob
+        self.expressno=expressno
+        self.debugdate=debugdate
+        self.deadline=deadline
+        self.contract=contract
+        self.invoice=invoice
+        self.getmoney=getmoney
+        self.close=close
+        self.info=info
+
+    def __str__(self):
+        return f'项目名字是{self.title}'
+
+
+class H5:
+    def __init__(self,webtitle,headline,icon,summary,music,loading,psd,share,data,url,api,monitor,duration;cdn,authorization,multilink,maintenance,h5info):
+        self.webtitle=webtitle
+        self.headline=headline
+        self.summary=summary
+        self.icon=icon
+        self.music
+        self.loading=loading
+        self.psd=psd
+        self.share=share
+        self.data=data
+        self.url=url
+        self.api=api
+        self.monitor=monitor
+        self.duration=duration
+        self.cdn=cdn
+        self.authorization=authorization
+        self.multilink=multilink
+        self.maintenance=maintenance
+        self.info=info
+
+
+
+
--- run.py
+++ run.py
@@ -1,16 +1,19 @@
-from flask import Flask
+from flask import Flask,render_template
+from models.project import Project
+
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template('index.html')
 
 
-@app.route('/nihao/')
-def nihao():
-    return 'ni hao'    
+@app.route('/projects_list/')
+def projects_list():
+    project=Project('OA','大地惊雷','panda',None,0.03,0,0,0,0,0,0,0,0,0,'pangzhilei',None,None,None,None,None,None,None,None,None,False,False,None)
+    return render_template('projects_list.html', project=project)    
 
 
 if __name__ == '__main__':
